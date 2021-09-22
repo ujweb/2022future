5321fbe502f1bf6b343409abd23f3443dfdf6ea1
yutingh <yutingh@cw.com.tw>
2021-09-22 08:25:18 +0800

    firebase demo

---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+	databaseURL: "https://future-57c0b-default-rtdb.firebaseio.com/"
+});
+const database = firebase.database();
+
+// database.ref('/').set('');
+
+// database.ref('/').once('value',e=>{
+//     console.log(e.val());
+// });
